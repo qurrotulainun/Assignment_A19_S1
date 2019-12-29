ceba535d3e1b066d33a7e3ad57e3e7953c0ce186
Qurrotul Ainun Ni'matus Sholihah <59301024+qurrotulainun@users.noreply.github.com>
2019-12-30 00:45:41 +0700

    Genap Ganjil
    Modulus 2

---
+++ Nilai Genap Ganjil.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+main()
+{
+	int nilai;
+	cout<<"Masukkan Nilai=";
+	cin>>nilai;
+		if(nilai%2==0)
+		cout<<"adalah bilangan genap";
+		else
+		cout<<"adalah bilangan ganjil";
+	return 0;
+}
