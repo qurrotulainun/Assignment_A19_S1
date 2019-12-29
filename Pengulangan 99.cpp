f3fb3f9c7641d1a88fc78b07dbcfddc5cc6be7ed
Qurrotul Ainun Ni'matus Sholihah <59301024+qurrotulainun@users.noreply.github.com>
2019-12-30 02:25:30 +0700

    Looping != 99

---
+++ Pengulangan 99.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int i = 0;
+	while (i !=99)
+	{
+		cout<<"Masukkan Sebuah Bilangan :";
+		cin>>i;
+		cout<<"Bilangan Anda Adalah="<<i<<endl;	
+	}
+}
